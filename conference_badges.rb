3f2d61b6c4afbb492edc8f0c148db3b4a31358a4
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-03-21 13:34:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(speakers)
   rooms = Array.new
     speakers.each_with_index do |speaker, room|
-      binding.pry
+      #binding.pry
       rooms <<  "You'll be assigned to room #{room}!"
   end
 end
